2386d4f2c472bd339cb06752655468075a574096
Gustavo <jg_barreto@hotmail.com>
2024-05-18 16:00:20 -0400

    separed widget in class

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -1,24 +1,29 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 25, 2, 80),
-              Color.fromARGB(255, 45, 7, 98)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-            child: const Center(
-              child: Text("Hello Miami",
-                  style: TextStyle(color: Colors.white, fontSize: 28)),
-            )),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 25, 2, 80),
+          Color.fromARGB(255, 45, 7, 98)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: const Center(
+          child: Text("Hello Miami",
+              style: TextStyle(color: Colors.white, fontSize: 28)),
+        ));
+  }
 }

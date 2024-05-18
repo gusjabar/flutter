2a9b549bedde4904489a18210ae605af44fe3bec
Gustavo <jg_barreto@hotmail.com>
2024-05-18 17:04:42 -0400

    add multiple const to a class

--- roll_dice/lib/custom_widget/gradient_container.dart
+++ roll_dice/lib/custom_widget/gradient_container.dart
@@ -2,22 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice/custom_widget/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(
-      // {super.key, required this.color1, required this.color2});
-      {
-    super.key,
-    required this.colors,
-  });
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  // final Color color1;
-  // final Color color2;
-  final List<Color> colors;
+  const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 91, 73, 132),
+        color2 = const Color.fromARGB(255, 45, 7, 98);
+
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
-                colors: colors,
+                colors: [color1, color2],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight)),
         child: const Center(
--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -5,12 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          colors: [
-            Color.fromARGB(255, 25, 2, 80),
-            Color.fromARGB(255, 45, 7, 98)
-          ],
-        ),
+        body: GradientContainer.purple(),
       ),
     ),
   );

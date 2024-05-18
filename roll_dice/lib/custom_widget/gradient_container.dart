0041235bfbfffeaf03a5e5d4ed16423b51330b8d
Gustavo <jg_barreto@hotmail.com>
2024-05-18 16:51:31 -0400

    working with class and var

--- roll_dice/lib/custom_widget/gradient_container.dart
+++ roll_dice/lib/custom_widget/gradient_container.dart
@@ -2,16 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice/custom_widget/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(
+      // {super.key, required this.color1, required this.color2});
+      {
+    super.key,
+    required this.colors,
+  });
 
+  // final Color color1;
+  // final Color color2;
+  final List<Color> colors;
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 25, 2, 80),
-          Color.fromARGB(255, 45, 7, 98)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: colors,
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight)),
         child: const Center(
           child: StyledText("Hello Miami (305)"),
         ));

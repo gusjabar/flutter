c0b61e3e9f7ea6316b9a28b61dbb05b964974b86
Gustavo <jg_barreto@hotmail.com>
2024-05-19 10:36:46 -0400

    quiz-app: added welcome page with image, icon button and label.

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: (LinearGradient(colors: [
+            Color.fromRGBO(103, 2, 110, 1),
+            Color.fromRGBO(217, 81, 227, 1)
+          ])),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Image.asset(
+                  "assets/images/quiz-logo.png",
+                  width: 300,
+                ),
+              ),
+              const Text(
+                "Hello",
+                style: TextStyle(fontSize: 28, color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_right),
+                  label: const Text("Start Quiz"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  ));
+}

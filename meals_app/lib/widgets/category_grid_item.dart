54c355190f97bbf0a591baaa5df1794f9dd35511
Gustavo <jg_barreto@hotmail.com>
2024-05-22 19:34:06 -0400

    added meals

--- meals_app/lib/widgets/category_grid_item.dart
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -9,23 +9,29 @@ class CategoryGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Container:perfect for background color and combinations.
-    return Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.6),
-              category.color.withOpacity(0.9)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
+    // GestureDetector: you will not get feedback with InkWell you do
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  category.color.withOpacity(0.6),
+                  category.color.withOpacity(0.9)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
-        ));
+              borderRadius: BorderRadius.circular(16)),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          )),
+    );
   }
 }

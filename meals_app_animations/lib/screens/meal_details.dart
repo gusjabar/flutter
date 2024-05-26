558603a6f7ae95d5990f1f0d0b0d1c079fb312b9
Gustavo <jg_barreto@hotmail.com>
2024-05-26 14:34:29 -0400

    animation between widgets

--- meals_app_animations/lib/screens/meal_details.dart
+++ meals_app_animations/lib/screens/meal_details.dart
@@ -32,17 +32,33 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+                //implicit animation
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) => RotationTransition(
+                      turns:
+                          Tween<double>(begin: 0.8, end: 1).animate(animation),
+                      child: child,
+                    ),
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(
+                      isFavorite), //key allow to detect that something change.
+                )),
           )
         ]),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                //animate across widgets
+                tag: meal.id, //same tag in meal_item.
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
